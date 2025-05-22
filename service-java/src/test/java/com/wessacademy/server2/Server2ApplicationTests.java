a0360b4e3d3da01aa1ad255a675242f6f55bf150
Tabai Iyadh <tabaiiyadh317@gmail.com>
2025-05-22 18:47:42 +0100

    AC-17 ai generated summary

--- service-java/src/test/java/com/wessacademy/server2/Server2ApplicationTests.java
+++ service-java/src/test/java/com/wessacademy/server2/Server2ApplicationTests.java
@@ -11,3 +11,22 @@ class Server2ApplicationTests {
     }
 
 }
+
+/**
+ * This is a test class for Server2Application
+ * It contains basic integration tests for the Spring Boot application
+ * 
+ * @author Your Name
+ * @version 1.0
+ * @since 2023
+ */
+
+/**
+ * The @SpringBootTest annotation indicates that this is a Spring Boot test class
+ * It will load the complete application context for integration testing
+ */
+
+/**
+ * contextLoads() is a basic test that verifies if the Spring application context loads successfully
+ * If this test passes, it means the basic configuration of the application is correct
+ */
